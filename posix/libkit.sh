barroit/barroit

commit deb8e7a418d29d9f0b151ea65d6c1312193a8d9b

posix/libkit: introduce ini_section*()
Signed-off-by: Jiamu Sun <barroit@linux.com>

posix/libkit.sh
@@ -136,3 +136,19 @@ line_need_skip()
 		return 1
 	esac
 }
+
+ini_section()
+{
+	perl -ne "print if /^\[$1\]$/ .. /^\[end$1\]$/ and !/^\[.*\]$/" $2
+}
+
+ini_section_st()
+{
+	ret=$(section $1 $2)
+
+	if [ -z "$ret" ]; then
+		die "no matching section $1 found in $2"
+	fi
+
+	printf '%s\n' "$ret"
+}

commit 5aacc99d0788ba68cb61d7ca9bd9622a7e1aca8c

posix/libkit: fix incorrect init_section() name
Fixes: deb8e7a418d2 ("posix/libkit: introduce ini_section*()")
Signed-off-by: Jiamu Sun <barroit@linux.com>

posix/libkit.sh
@@ -144,7 +144,7 @@ ini_section()
 
 ini_section_st()
 {
-	ret=$(section $1 $2)
+	ret=$(ini_section $1 $2)
 
 	if [ -z "$ret" ]; then
 		die "no matching section $1 found in $2"