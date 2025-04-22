44c14a5cd1d69abe1a7f104024e4ea1118b30200
Jiamu Sun <barroit@linux.com>
2025-04-22 17:22:42 +0900

    scripts/use-libkit.sh: clarify script usage
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/use-libkit.sh
+++ scripts/use-libkit.sh
@@ -3,11 +3,12 @@
 
 # To quick set up libkit.sh in your project
 #
-#	Symlink scripts/libkit.sh against this/root/current/libkit.sh
-#	Symlink scripts/use-libkit.sh against this/script
-#	Source scripts/use-libkit.sh in other scripts
+#   1 - symlink scripts/libkit.sh against this/root/current/libkit.sh
+#   2 - symlink scripts/use-libkit.sh against this/script
+#   3 - source scripts/use-libkit.sh in other scripts
 #
-# NB: to gain the maximum compatibility, use use-posix-libkit.sh when possible
+# NB: cwd must be $(dirname scripts/libkit.sh)/..
+#     to gain the maximum compatibility, use use-posix-libkit.sh when possible
 
 libkit=scripts/libkit.sh
 libkit=$(perl -e 'use Cwd "abs_path"; print abs_path(shift); "\n"' $libkit)
