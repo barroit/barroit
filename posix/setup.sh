itsconnner/itsconnner

commit 37bb49a3f2fa27da266ff7cca027fec70854dc51

unix: implement script execution workflow
Signed-off-by: Jiamu Sun <barroit@linux.com>

posix/setup.sh
@@ -1,0 +1,55 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+absname()
+{
+	printf %s $(perl -e 'use Cwd "abs_path"; print abs_path(shift)' $1)
+}
+
+script_root=$(absname $(dirname $0))
+libkit_root=$(dirname $(absname $0))
+
+. $libkit_root/libkit.sh
+
+if [ -n "$1" ]; then
+	name=$1
+
+	if [ $(printf %.1s $name) = '+' ]; then
+		force=1
+		name=${name#+}
+	fi
+
+	case $name in
+	[0-9][0-9]-*)
+		scripts=$(find $script_root -name $name | sort)
+		;;
+	*)
+		scripts=$(find $script_root -name [0-9][0-9]-$name*.sh | sort)
+		;;
+	esac
+
+	if [ -z "$scripts" ]; then
+		die "unknown script \`$name'"
+	fi
+
+	if [ $(printf '%s\n' $scripts | wc -l) -ne 1 ]; then
+		lines=$(printf '%s\n' $scripts | xargs -n1 basename)
+		lines=$(printf '\n  %s' $lines)
+
+		die "ambiguous script \`$name'; could be:$lines"
+	fi
+
+	if [ $force ]; then
+		export FORCE_EXEC=$scripts
+	fi
+fi
+
+scripts=${scripts:-$(find $script_root -type f -name [0-9][0-9]-*.sh | sort)}
+
+for script in $scripts; do
+	$script_root/exec.sh $script
+
+	if [ $? -ne 0 ]; then
+		warn "$(basename $script) interrupted"
+	fi
+done

ubuntu/setup.sh
@@ -1,0 +1,1 @@
+../posix/setup.sh

linux/exec.sh
@@ -1,0 +1,19 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+export SCRIPT_PATH=$1
+export SCRIPT_ROOT=$(dirname $0)
+export CONFIG_ROOT=$SCRIPT_ROOT/../config
+export LIBKIT_ROOT=$(dirname $(readlink -f $0))
+
+function datadir()
+{
+	printf %s $HOME/.local/share/barroit
+}
+
+mkdir -p $(datadir)
+
+source $LIBKIT_ROOT/libkit.sh
+source $LIBKIT_ROOT/libsetup.sh
+
+source $1

linux/libsetup.sh
@@ -1,0 +1,1 @@
+../posix/libsetup.sh

posix/libsetup.sh
@@ -1,0 +1,21 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+setup_is_done()
+{
+	touch $(datadir)/done
+
+	grep -q $SCRIPT_PATH $(datadir)/done
+}
+
+setup_done()
+{
+	if ! setup_is_done; then
+		printf '%s\n' $SCRIPT_PATH >> $(datadir)/done
+	fi
+}
+
+exec_is_foce()
+{
+	test $SCRIPT_PATH = "$FORCE_EXEC"
+}

ubuntu/exec.sh
@@ -1,0 +1,1 @@
+../linux/exec.sh

barroit/barroit

commit 12f012c4b73c53e390b0f51a5e0fbb3f925d37f8

posix/setup: remove file filter in find command
Fixes: 37bb49a3f2fa ("unix: implement script execution workflow")
Signed-off-by: Jiamu Sun <barroit@linux.com>

posix/setup.sh
@@ -44,7 +44,7 @@ if [ -n "$1" ]; then
 	fi
 fi
 
-scripts=${scripts:-$(find $script_root -type f -name [0-9][0-9]-*.sh | sort)}
+scripts=${scripts:-$(find $script_root -name [0-9][0-9]-*.sh | sort)}
 
 for script in $scripts; do
 	$script_root/exec.sh $script