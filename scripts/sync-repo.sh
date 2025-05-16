108783bccef919b62e4b48e055db87348388a23e
Jiamu Sun <barroit@linux.com>
2025-05-17 00:10:47 +0900

    scripts/sync-repo: support switching branch on another device
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/sync-repo.sh
+++ scripts/sync-repo.sh
@@ -3,6 +3,8 @@
 
 set -e
 
+branch=$(git branch --show-current)
+
 git push
 
 git add .
@@ -20,8 +22,10 @@ set -e
 cd ~/${PWD#$HOME/}
 export PATH=\$HOME/.local/bin:\$PATH
 
+git switch $branch
+
 git pull
-git reset --hard origin/master
+git reset --hard \$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
 
 git am -
 git reset HEAD^
