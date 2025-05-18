fbb745b38c792f9443f2e61d1b67ccbeda1665e8
Jiamu Sun <barroit@linux.com>
2025-05-19 01:43:08 +0900

    scripts/mount-wasabi: enable full VFS cache for wasabi:barroit
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/mount-wasabi.sh
+++ scripts/mount-wasabi.sh
@@ -4,7 +4,7 @@
 mkdir -p $HOME/wasabi $HOME/cred
 
 if ! mount | grep -q wasabi:barroit; then
-	rclone mount --daemon wasabi:barroit $HOME/wasabi
+	rclone mount --daemon --vfs-cache-mode=full wasabi:barroit $HOME/wasabi
 fi
 
 if ! mount | grep -q wasabi:cred; then
