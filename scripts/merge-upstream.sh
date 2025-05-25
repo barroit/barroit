82280c3b63f3a77e46b01f60284cd3099d4e7d68
Jiamu Sun <barroit@linux.com>
2025-05-25 15:55:13 +0900

    scripts/pull-upstream: rework synckit
    This commit reworks synckit to support embedding a changelog section
    in commit messages. This makes merge commits more structured and helps
    track changes more easily.
    
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ scripts/merge-upstream.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -e
+
+script_path=$(perl -e 'use Cwd "abs_path"; print abs_path(shift); "\n"' $0)
+script_root=$(dirname $script_path)
+
+. $script_root/../posix/libkit.sh
+
+if [ -z "$1" ]; then
+	die 'missing ref'
+fi
+
+if [ -n "$(git status --short)" ]; then
+	die "stash your worktree and index"
+fi
+
+ref=$1
+branch=$(git branch --show-current)
+base=$(git merge-base $branch $ref)
+
+if [ $base = $(git rev-parse $ref) ]; then
+	printf 'nothing to merge.\n'
+	exit
+fi
+
+top=$(git log --pretty='%h' -1 $ref)
+history=$(git log --format='%s' $base..$ref | grep :)
+gitdir=$(git rev-parse --git-dir)
+
+cat <<EOF > $gitdir/MERGE_MSG.1
+Merge upstream $ref
+
+This commit merges upstream $ref at $top.
+
+======== CHANGELOG ========
+
+$(printf '%s\n' "$history" | $script_root/group-history.py)
+
+Signed-off-by: $(git var GIT_AUTHOR_IDENT | cut -d' ' -f-3)
+EOF
+
+cp $gitdir/MERGE_MSG.1 $gitdir/MERGE_MSG
+
+if git merge --no-ff --no-commit --no-edit $ref; then
+	do_ff=1
+elif [ -f .pickignore ]; then
+	for file in $(cat .pickignore); do
+		if [ -f $file ]; then
+			git rm $file
+		fi
+	done
+fi
+
+if [ -f FIXLICENSE ]; then
+	new=$(v2 FIXLICENSE new)
+	old=$(v2 FIXLICENSE old)
+
+	$root/scripts/fix-license.sh "$old" "$new"
+fi
+
+cp $gitdir/MERGE_MSG.1 $gitdir/MERGE_MSG
+
+if [ -z "$(git diff --name-only --diff-filter=U)" ] || [ $do_ff ]; then
+	git add .
+	git merge --continue
+fi
