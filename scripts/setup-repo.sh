3b531d4ef9e49eff4a470c852d074cfb00e01acb
Jiamu Sun <barroit@linux.com>
2025-05-26 02:14:17 +0900

    scripts/setup-repo: allow setup in non-barroit repo
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/setup-repo.sh
+++ scripts/setup-repo.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-ln -sf ../../hooks/pre-commit.sh .git/hooks/pre-commit
-ln -sf ../../hooks/prepare-commit-msg.sh .git/hooks/prepare-commit-msg
+ln -sf ../../../barroit/hooks/pre-commit.sh \
+       .git/hooks/pre-commit
+
+ln -sf ../../../barroit/hooks/prepare-commit-msg.sh \
+       .git/hooks/prepare-commit-msg
