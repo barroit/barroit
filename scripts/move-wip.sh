75284f6e1f86ceca7b10c1e0114bd688dac6ab54
Jiamu Sun <barroit@linux.com>
2025-05-25 17:11:04 +0900

    scripts/move-wip: enforce same remote across devices
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/move-wip.sh
+++ scripts/move-wip.sh
@@ -18,19 +18,26 @@ else
 	host=dev.ubuntu
 fi
 
+upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+remote=${upstream%%/*}
+branch=${upstream#*/}
+remote_url=$(git remote get-url $remote)
+
 git format-patch HEAD~1 --stdout | ssh $host "
 set -e
 
 cd \$HOME/${PWD#$HOME/}
 export PATH=\$HOME/.local/bin:\$PATH
 
-git switch $branch
+if git remote | grep -q move-wip; then
+	git remote set-url move-wip $remote_url
+else
+	git remote add move-wip $remote_url
+fi
 
-track=\$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
-remote=\${track%%/*}
-
-git fetch \$remote $branch
-git reset --hard \$track
+git fetch move-wip $branch
+git switch -C $branch
+git reset --hard move-wip/$branch
 
 git am -
 git reset HEAD^
