e367e153de451f1f6a1ed91add01086de458bff8
Jiamu Sun <barroit@linux.com>
2025-06-29 01:10:48 +0900

    react: make general-purpose parts reusable
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ scripts/styles-fix.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -e
+
+mkdir -p build
+
+# PostCSS follows symlinks by their *smart* design, so use hardlink to
+# block that dumb behavior.
+cp -RL styles/. build/styles
