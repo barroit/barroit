d427812f107983fff23a3a4a19cd8e18d2c551b3
Jiamu Sun <barroit@linux.com>
2025-07-15 11:48:57 +0900

    scripts/merge-upstream: include remote URL in commit message
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/merge-upstream.sh
+++ scripts/merge-upstream.sh
@@ -12,38 +12,76 @@ if [ -z "$1" ]; then
 	die 'missing ref'
 fi
 
-if [ -n "$(git status --short)" ]; then
-	die "stash your worktree and index"
+if ! git rev-parse --verify --quiet $1 >/dev/null; then
+	die "invalid ref '$1'"
 fi
 
-ref=$1
-branch=$(git branch --show-current)
-base=$(git merge-base $branch $ref)
+if [ -n "$(git status --short)" ]; then
+	die 'stash your worktree and index'
+fi
 
-if [ $base = $(git rev-parse $ref) ]; then
-	printf 'nothing to merge.\n'
+that=$(git rev-parse --symbolic-full-name $1)
+that_remote=$(git for-each-ref --format='%(upstream:remotename)' $that)
+
+if [ -z "$that_remote" ]; then
+	name=$(git for-each-ref --format='%(refname:rstrip=-2)' $that)
+
+	if [ $name = refs/remotes ]; then
+		name=$(git for-each-ref --format='%(refname:rstrip=-3)' $that)
+		that_remote=${name##*/}
+	fi
+fi
+
+if [ -z "$that_remote" ]; then
+	that_remote_line='That-remote: NULL'
+else
+	that_remtoe_url=$(git remote get-url $that_remote)
+	that_remote_line="That-remote: $that_remtoe_url ($that_remote)"
+fi
+
+this=$(git branch --show-current)
+base=$(git merge-base $this $that)
+
+this_ref=$(git rev-parse --symbolic-full-name $this)
+this_remote=$(git for-each-ref --format='%(upstream:remotename)' $this_ref)
+
+if [ -z "$this_remote" ]; then
+	this_remote_line='This-remote: NULL'
+else
+	this_remtoe_url=$(git remote get-url $this_remote)
+	this_remote_line="This-remote: $this_remtoe_url ($this_remote)"
+fi
+
+if [ -z "$this_remote" ]; then
+	this_remote=NULL
+fi
+
+if [ $base = $(git rev-parse $that^{commit}) ]; then
+	printf 'nothing to merge\n'
 	exit
 fi
 
-top=$(git log --pretty='%h' -1 $ref)
-history=$(git log --format='%s' $base..$ref | grep :)
+top=$(git log --pretty='%h' -1 $this)
+history=$(git log --format='%s' $base..$that | grep :)
 gitdir=$(git rev-parse --git-dir)
 
 cat <<EOF > $gitdir/MERGE_MSG.1
-Merge upstream $ref
+Merge $1 into $this
 
-This commit merges upstream $ref at $top.
+This commit merges ${that#*/} at $top.
 
 ======== CHANGELOG ========
 
 $(printf '%s\n' "$history" | $script_root/group-history.py)
 
+$that_remote_line
+$this_remote_line
 Signed-off-by: $(git var GIT_AUTHOR_IDENT | cut -d' ' -f-3)
 EOF
 
 cp $gitdir/MERGE_MSG.1 $gitdir/MERGE_MSG
 
-if git merge --no-ff --no-commit --no-edit $ref; then
+if git merge --no-ff --no-commit --no-edit $that; then
 	do_ff=1
 elif [ -f .pickignore ]; then
 	for file in $(cat .pickignore); do
