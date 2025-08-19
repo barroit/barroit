d6f307b5be2e59984a83e0eba820f897ea50bf70
Jiamu Sun <barroit@linux.com>
2025-08-19 12:43:12 +0900

    scripts/diff-path: move path diff code to helper
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/setup-repo.sh
+++ scripts/setup-repo.sh
@@ -4,12 +4,7 @@
 this=$(perl -e 'use Cwd "abs_path"; print abs_path(shift); "\n"' $0)
 root=$(dirname $this)/..
 hooks=$(git rev-parse --git-dir)/hooks
-
-src=$(cat <<-EOF | perl -l - $root $hooks
-	require File::Spec;
-	print File::Spec->abs2rel(shift, shift);
-EOF
-)/hooks
+src=$($root/scripts/diff-path.pl $root $hooks)/hooks
 
 ln -sf $src/pre-commit.sh $hooks/pre-commit
 ln -sf $src/prepare-commit-msg.sh $hooks/prepare-commit-msg
